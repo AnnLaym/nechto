a55c85f3c6e96c267e61c4e99eb8c4c7112b2a7f
annlaym <rekohoo@gmail.com>
2023-09-13 01:52:02 +0300

    fix 1.0.2.0

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
-    base: '/timeline',
+    base: '/nechto',
     build: {
         // generate manifest.json in outDir
         manifest: true,
